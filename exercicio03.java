9a3125803d0d99d49cd5c0106123c73648119a39
glaucia-desing <56209111+glaucia-desing@users.noreply.github.com>
2022-04-06 21:45:08 -0300

    Add files via upload

---
+++ exercicio03.java
@@ -0,0 +1,19 @@
+package com.company;
+//*Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+import java.util.Scanner;
+
+public class exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" digite uma letra ");
+        char letra = scanner.nextLine().charAt(0);
+        if ('F' = letra // 'f' = letra ){
+            System.out.println(" feminino ");
+    }else if ('M' = letra // 'm' = letra )
+            System.out.println(" masculino ");
+    }else{
+        System.out.println(" sexo invalido ");
+
+        }
+
+
